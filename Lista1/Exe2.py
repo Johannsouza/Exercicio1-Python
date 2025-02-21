0e52a20495e2917c268e2c07f6e76331642b1933
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 19:45:59 -0300

    Exe2.py

---
+++ Lista1/Exe2.py
@@ -0,0 +1,7 @@
+#2 - Faça um programa que leia uma temperatura em graus Celsius e a converta para Fahrenheit.
+
+#C = int(input("Escreva a temperatura em Celsius: "))
+
+#F = (C * 9/5) + 32
+
+#print("O valor em Fahrenheit é: " + F)
