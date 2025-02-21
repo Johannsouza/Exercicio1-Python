65ec3492ceff77d84a9f346e044a224fcd0f7736
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 19:47:19 -0300

    Exe5.py

---
+++ Lista1/Exe5.py
@@ -0,0 +1,6 @@
+# 5 - Desenvolva um programa que receba um valor em metros e o converta para centímetros.
+
+metro = float(input("Digite o valor em metros: "))
+centimetro=metro*100
+
+print("O valor em centrímetro é: " + str(centimetro))
