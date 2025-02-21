7139324820c759f444e7a6052455e90ddbffdb15
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 19:46:44 -0300

    Exe4.py

---
+++ Lista1/Exe4.py
@@ -0,0 +1,12 @@
+#4 - Faça um programa que pergunte o número de horas trabalhadas no mês e o valor recebido  por hora. O programa deve calcular e exibir o salário total do mês.
+
+#horas = float(input("Digite a quantidade de horas trabalhadas: "))
+
+#valor =  float(input("Digite o valor recebido por hora: "))
+
+#salario=horas*valor
+
+# if(horas>730):
+#     print("É necessário calcular o salário total do mês. As horas ultrapassam o mês.")
+# else:
+#     print(salario)
