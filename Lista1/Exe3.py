8db98f52636941f050e68f7992d2f1ba2ff213db
Johannsouza <112652745+Johannsouza@users.noreply.github.com>
2025-02-21 19:46:25 -0300

    Exe3.py

---
+++ Lista1/Exe3.py
@@ -0,0 +1,9 @@
+# 3 - Crie um programa que receba três números do usuário e calcule a média aritmética deles.
+
+#n1 = float(input("Digite um Valor: "))
+#n2 = float(input("Digite um Valor: "))
+#n3 = float(input("Digite um Valor: "))
+
+#resultado = n1+n2+n3/3
+
+#print(resultado)
